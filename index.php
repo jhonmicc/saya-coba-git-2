c25c0cf163c686b3f2224cb54b0348586dda2db5
jhonmicc <jhonmichaelmanik@gmail.com>
2021-08-06 13:12:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,14 @@
 <body>
     <h1>Maafkan Saya</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolorem exercitationem ullam repudiandae reiciendis amet nihil eveniet perspiciatis, animi labore nesciunt architecto esse iste pariatur nobis harum enim ducimus porro odit incidunt laudantium at! Dolorem accusantium laboriosam, saepe numquam aut sunt quasi accusamus reprehenderit! Suscipit laudantium in error quae, ea eius sed explicabo neque ullam maxime nam cupiditate atque, velit laborum. Excepturi quam mollitia voluptatum quod ea asperiores temporibus atque quidem ipsam eligendi sunt enim minima, explicabo facere vel placeat maxime, architecto, esse ipsum. Nostrum quis ab odit illo voluptatum, quam fuga necessitatibus distinctio officiis velit sequi similique, reiciendis aperiam!</p>
+
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+        <li>Tiga</li>
+        <li>Empat</li>
+        <li>Lima</li>
+    </ul>
 </body>
 
 </html>
